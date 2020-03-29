73b8e73330fc515ec9bb2faf4c7ffeae1bfd756a
hanszimmer <michael.weitnau@live.de>
2020-03-29 12:40:04 +0200

    add decline

--- elixir/check.ex
+++ elixir/check.ex
@@ -28,15 +28,39 @@ defmodule Check do
         end
     end
 
-    def check_incline([row | rest_rows]) do
+    def check_incline(rows) when (length rows) < 4 do
         false
     end
 
-    def check_decline([row | rest_rows]) do
+    def check_incline([row_1,row_2,row_3,row_4 | rest_rows]) do
+        # Traverse columns to rows
+
+        # use check_horizontal
+        if _check_incline(row_1,row_2,row_3,row_4) do
+            true
+        else
+            check_incline([row_2,row_3,row_4|rest_rows])
+        end
+    end
+
+    def check_decline(rows) when (length rows) < 4 do
         false
     end
 
+    def check_decline([row_1,row_2,row_3,row_4 | rest_rows]) do
+        # Traverse columns to rows
+
+        # use check_horizontal
+        if _check_decline(row_1,row_2,row_3,row_4) do
+            true
+        else
+            check_decline([row_2,row_3,row_4|rest_rows])
+        end
+    end
+
+    ##################
     # HELPER FUNCTIONS
+    ##################
 
     def _check_horizontal([ elem, elem, elem, elem | _row_rest ]) when elem != "-" do
         true
@@ -64,6 +88,30 @@ defmodule Check do
     end
 
 
+    def _check_incline([elem | _], [_, elem | _], [_,_,elem | _], [_,_,_,elem | _]) when elem != "-" do
+        true
+    end
+
+    def _check_incline([_ | rest_1], [_ | rest_2], [_ | rest_3], [_ | rest_4]) do
+        _check_incline(rest_1, rest_2, rest_3, rest_4)
+    end
+
+    def _check_incline(_,_,_,_) do
+        false
+    end
+
+
+    def _check_decline([_,_,_,elem | _], [_,_,elem | _], [_,elem | _], [elem | _]) when elem != "-" do
+        true
+    end
+
+    def _check_decline([_ | rest_1], [_ | rest_2], [_ | rest_3], [_ | rest_4]) do
+        _check_decline(rest_1, rest_2, rest_3, rest_4)
+    end
+
+    def _check_decline(_,_,_,_) do
+        false
+    end
 
 
     def get_elem([head | _ ], 0) do
